82c50033b13596794cbe811ce44e5c2d2583d1da
maguelt <88099556+maguelt@users.noreply.github.com>
2021-07-28 15:39:24 -0500

    Add files via upload

---
+++ tarea1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+x=float(-1)
+print(type(x))
+y=3*x**3-2*x**2+3*x-1
+print(float(y))
+
+x=float(0)
+print(type(x))
+y=3*x**3-2*x**2+3*x-1
+print(float(y))
+
+x=float(1)
+print(type(x))
+y=3*x**3-2*x**2+3*x-1
+print(float(y))
+
+
